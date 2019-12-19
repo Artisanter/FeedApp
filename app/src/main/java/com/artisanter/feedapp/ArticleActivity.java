8d8784fc4444d1f4d753da980389656a466825d1
Artisanter <chyzhyk99@gmail.com>
2019-12-19 03:01:52 +0300

    Width fix

--- app/src/main/java/com/artisanter/feedapp/ArticleActivity.java
+++ app/src/main/java/com/artisanter/feedapp/ArticleActivity.java
@@ -13,9 +13,13 @@ public class ArticleActivity extends NetworkActivity {
         Bundle extras = Objects.requireNonNull(getIntent().getExtras());
         setContentView(R.layout.activity_article);
 
-
         ((TextView)findViewById(R.id.title)).setText(extras.getString("Title"));
         ((WebView)findViewById(R.id.description))
-                .loadData(extras.getString("Description"), "text/html", null);
+                .loadDataWithBaseURL(null,
+                        "<style>img{display: inline;height: auto;max-width: 100%;}</style>"
+                                + extras.getString("Description")
+                        , "text/html"
+                        , "UTF-8"
+                        , null);
     }
 }
